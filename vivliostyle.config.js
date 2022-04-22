848ed3eae6358d26552ebbd2a41a5aecf08b5952
ogwata <ogwata@vivliostyle.org>
2022-04-22 12:05:07 +0900

    themeのpath変更

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'slide-20220423-2',
   author: '小形克宏 <ogwata@vivliostyle.org>', 
-  theme: 'themes/mytheme-slide', 
+  theme: 'themes/mytheme-slide/theme_common.css', 
   entry: [
     'myslide.md', 
       ], 
